6ec48cf1686a7f0949826ff5a8a7fa9d49bf80d7
NobruLcnITS <bruno.luciano@itsrizzoli.it>
2024-10-18 15:31:20 +0000

    main e untitled

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import os
 
+print('############')
+def list_files_and_directories(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * level
+        print(f'{indent}{os.path.basename(root)}/')
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print(f'{subindent}{f}')
+            
 model_dir = os.environ['SM_MODEL_DIR']
 
-with open(model_dir + '/output.txt', 'w') as f:
-    f.write('Ciao')
+with open(model_dir + '/output_model.txt', 'w') as f:
+    f.write('Ciao sono il modello')
     
 output_dir = os.environ['SM_OUTPUT_DIR']
 with open(output_dir + '/output.txt', 'w') as f:
-    f.write('Ciao')
+    f.write('Ciao sono i log del terminale')
+    
+input_dir = os.environ['SM_INPUT_DIR']
+list_files_and_directories(input_dir)
+
+with open(input_dir + "/data/training/train.csv", 'r') as fp:
+    lines = len(fp.readlines())
+    print('######### Total Number of lines:', lines)
+    with open(output_dir + "/output_lines.txt", 'w') as f:
+        f.write(f'CIao le line sono: {lines}')
